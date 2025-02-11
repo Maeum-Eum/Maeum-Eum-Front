19c212c71a88037358ad482f60bd776cdd6ec815
zziyuni <86959644+zziyuni@users.noreply.github.com>
2025-02-12 01:26:16 +0900

    App 파일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import styled, { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import { Layout } from './components/Layout';
+import { Outlet } from 'react-router-dom';
+import useMediaQuery from './hooks/useMediaQuery';
+import GlobalStyle from './styles/GlobalStyle';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
 function App() {
-  return <></>;
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.mobile})`);
+  return (
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <GlobalStyle isMobile={isMobile} />
+        <Layout isMobile={isMobile}>
+          <Outlet />
+        </Layout>
+      </Wrapper>
+    </ThemeProvider>
+  );
 }
 
 export default App;
