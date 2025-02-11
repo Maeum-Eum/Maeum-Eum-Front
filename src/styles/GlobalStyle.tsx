d8419555942fbe1f944823deff18e99591112e67
zziyuni <86959644+zziyuni@users.noreply.github.com>
2025-02-12 01:23:44 +0900

    Global style 설정

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from 'styled-components';
+
+interface GlobalStyleProps {
+  isMobile: boolean;
+}
+
+const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
+ @font-face {
+    font-family: 'PretendardRegular';
+    src: url('/src/assets/fonts/Pretendard-Regular.woff') format('truetype');
+  }
+  
+  @font-face {
+    font-family: 'PretendardMedium';
+    src: url('/src/assets/fonts/Pretendard-Medium.woff') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'PretendardSemiBold';
+    src: url('/src/assets/fonts/Pretendard-SemiBold.woff') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'PretendardBold';
+    src: url('/src/assets/fonts/Pretendard-Bold.woff') format('truetype');
+
+  }
+  @font-face {
+    font-family: 'PretendardLight';
+    src: url('/src/assets/fonts/Pretendard-Light.woff') format('truetype');
+
+  }
+html {
+  font-size: 62.5%; 
+}
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: PretendardRegular;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+
+  #root {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export default GlobalStyle;
