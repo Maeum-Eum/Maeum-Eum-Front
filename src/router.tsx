84b74e0d5f446583eb753f10190818e020b4082a
zziyuni <86959644+zziyuni@users.noreply.github.com>
2025-02-12 01:25:18 +0900

    라우터 추가

---
+++ src/router.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import { Login } from './pages/Login';
+import { NotFound } from './pages/NotFound';
+import { ProtectedRoute } from './components/ProtectedRoute';
+import { Home } from './pages/Home';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: 'login', element: <Login /> },
+      {
+        path: '',
+        element: <ProtectedRoute />,
+        children: [{ path: '', element: <Home /> }],
+      },
+    ],
+    errorElement: <NotFound />,
+  },
+]);
