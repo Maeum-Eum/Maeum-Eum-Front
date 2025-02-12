89535d8cc55db67c56dfaced2c8649605712ed65
zziyuni <86959644+zziyuni@users.noreply.github.com>
2025-02-12 22:25:17 +0900

    회원가입 각 항목 label 추가

---
+++ src/components/SignUp/SignUpLabel.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SignUpLabel = ({ label }: { label: string }) => {
+  return <Title>{label}</Title>;
+};
+
+const Title = styled.div`
+  ${({ theme }) => theme.fontStyles.bodyMediumSB}
+  margin-bottom: 1rem;
+  margin-top: 3rem;
+`;
