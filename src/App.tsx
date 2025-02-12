31e60a4729f41586a74fc08f00bd1e5dd1c0b9e1
zziyuni <86959644+zziyuni@users.noreply.github.com>
2025-02-12 23:12:22 +0900

    필요없는 Wrapper 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,14 @@ import { Outlet } from 'react-router-dom';
 import useMediaQuery from './hooks/useMediaQuery';
 import GlobalStyle from './styles/GlobalStyle';
 
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-`;
-
 function App() {
   const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.mobile})`);
   return (
     <ThemeProvider theme={theme}>
-      <Wrapper>
-        <GlobalStyle isMobile={isMobile} />
-        <Layout isMobile={isMobile}>
-          <Outlet />
-        </Layout>
-      </Wrapper>
+      <GlobalStyle isMobile={isMobile} />
+      <Layout isMobile={isMobile}>
+        <Outlet />
+      </Layout>
     </ThemeProvider>
   );
 }
