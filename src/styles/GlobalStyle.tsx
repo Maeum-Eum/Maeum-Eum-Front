b9f187c62a83aac2b8fe3bda0acbf6c00fb0a2d8
zziyuni <86959644+zziyuni@users.noreply.github.com>
2025-02-14 16:23:09 +0900

    폰트 사이즈 조절

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -31,10 +31,8 @@ const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
 
   }
 html {
-  font-size: ${(props) =>
-    props.isMobile
-      ? '62.5%'
-      : '120%'}; //TODO : 크기 임의 설정, 웹뷰 화면 크기 나오면 조정하기
+  font-size: 62.5%
+    
 }
   body {
     margin: 0;
