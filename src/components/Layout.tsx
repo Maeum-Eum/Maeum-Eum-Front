9347cfabdda6783e156f7981cc415f4b736ecf5f
zziyuni <86959644+zziyuni@users.noreply.github.com>
2025-02-14 22:17:26 +0900

    레이아웃 코드 정리

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,17 +6,16 @@ const Wrapper = styled.div<WrapperProps>`
   min-width: 320px; //모바일 크기
   width: 100%;
   margin: 0 auto;
+  min-height: 100dvh;
 
   @media (min-width: 768px) {
     //태블릿 사이즈
     max-width: 500px;
-    min-height: 100vh;
     height: 100%;
   }
 
   @media (min-width: 1024px) {
     //데스크탑 사이즈
-
     width: 500px;
   }
   background-color: ${(props) => props.theme.colors.background};
@@ -25,7 +24,6 @@ const Wrapper = styled.div<WrapperProps>`
     isMobile ? 'none' : '0 4px 20px rgba(0, 0, 0, 0.1)'};
   overflow: hidden;
   position: relative;
-  align-items: center;
 `;
 
 interface LayoutProps {
