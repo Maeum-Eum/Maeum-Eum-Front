8bb9454ea4138be03df72870ffeb970640e542f6
zziyuni <86959644+zziyuni@users.noreply.github.com>
2025-02-15 22:30:33 +0900

    회원가입 요양보호사/사회복지사 화면

--- src/components/SignUp/SignUpCareWorker.tsx
+++ src/components/SignUp/SignUpCareWorker.tsx
@@ -1,20 +1,67 @@
 import { RoundedButton } from './RoundedButton';
 import { Input } from '../Input';
 import { SignUpLabel } from './SignUpLabel';
-import { InputWrapper, SignUpLayout } from './SignUpLayout';
+import { ErrorText, InputWrapper, SignUpLayout } from './SignUpLayout';
+import { useSignUpStore } from '../../store/signUpStore';
+import { SelectedAddress } from '../address/SelectedAddress';
+import { useDaumPostcodePopup } from 'react-daum-postcode';
+import { hangeulInput, numInput } from '../../utils/regex';
 
 export const SignUpCareWorker = () => {
+  const { formData, updateFormData, errors } = useSignUpStore();
+
+  const postcodeScriptUrl =
+    'https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
+  const open = useDaumPostcodePopup(postcodeScriptUrl);
+
+  const handleComplete = (data: any) => {
+    updateFormData({
+      address: {
+        zonecode: data.zonecode,
+        roadAddress: data.roadAddress,
+        jibunAddress: data.jibunAddress,
+      },
+    });
+  };
+
+  const handleClick = () => {
+    open({ onComplete: handleComplete });
+  };
+
   return (
-    <SignUpLayout title="요양보호사님 정보 입력">
+    <SignUpLayout title="요양보호사님 정보 입력" require={true}>
       <SignUpLabel label="이름" />
-      <Input placeholder="이름을 입력해주세요" />
-      <SignUpLabel label="휴대전화" />
-      <Input placeholder="숫자만 입력해주세요" />
+      <Input
+        placeholder="이름을 입력해주세요"
+        value={formData.name}
+        maxLength={10}
+        onChange={(e) => updateFormData({ name: hangeulInput(e) })}
+      />
+      {errors.name && (
+        <ErrorText error={errors.name !== null}>{errors.name}</ErrorText>
+      )}
+      <SignUpLabel label="휴대전화 번호" />
+      <Input
+        placeholder="휴대전화 번호를 입력해주세요"
+        value={formData.phone}
+        maxLength={11}
+        onChange={(e) => updateFormData({ phone: numInput(e) })}
+      />
+      {errors.phone && (
+        <ErrorText error={errors.phone !== null}>{errors.phone}</ErrorText>
+      )}
       <SignUpLabel label="주소" />
       <InputWrapper>
-        <Input placeholder="주소를 입력해주세요" />
-        <RoundedButton text={'검색하기'} func={() => {}}></RoundedButton>
+        <Input placeholder="주소를 입력해주세요" disabled={true} />
+        <RoundedButton
+          text={'검색하기'}
+          func={() => handleClick()}
+        ></RoundedButton>
       </InputWrapper>
+      {errors.address && (
+        <ErrorText error={errors.address !== null}>{errors.address}</ErrorText>
+      )}
+      {formData.address.zonecode && <SelectedAddress {...formData.address} />}
     </SignUpLayout>
   );
 };
