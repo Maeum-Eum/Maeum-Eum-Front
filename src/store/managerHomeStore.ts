ce9d1028eac610a1c8f0fdb7c7f2cccc39057991
zziyuni <86959644+zziyuni@users.noreply.github.com>
2025-02-20 04:37:32 +0900

    상태관리

--- src/store/managerHomeStore.ts
+++ src/store/managerHomeStore.ts
@@ -1,13 +1,27 @@
 import { create } from 'zustand';
-import { IGetMainList } from '../services/home';
+import { IElder, IManagerMain } from '../services/home';
 
-interface CareGiverHomeState {
-  data: IGetMainList | null;
-  setData: (data: IGetMainList) => void;
+interface ManagerHomeState {
+  data: IManagerMain[] | null;
+  setData: (data: IManagerMain[]) => void;
+  toggleBookmark: (caregiverId: number) => void;
+  elderList: IElder[];
+  setElderList: (data: IElder[]) => void;
 }
 
-export const useCareGiverHomeStore = create<CareGiverHomeState>((set) => ({
+export const useManagerHomeStore = create<ManagerHomeState>((set) => ({
   data: null,
-
   setData: (data) => set({ data }),
+  toggleBookmark: (caregiverId) =>
+    set((state) => ({
+      data: state.data
+        ? state.data.map((item) =>
+            item.caregiverId === caregiverId
+              ? { ...item, isBookmarks: !item.isBookmarks }
+              : item
+          )
+        : null,
+    })),
+  elderList: [],
+  setElderList: (elderList) => set({ elderList }),
 }));
