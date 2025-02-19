85e54b0e7580dbea7f71080f391c2f876136909d
zziyuni <86959644+zziyuni@users.noreply.github.com>
2025-02-19 16:32:15 +0900

    Not found 페이지 디자인 수정

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,29 @@
+import styled from 'styled-components';
+
 export const NotFound = () => {
-  return <div>404</div>;
+  return (
+    <Wrapper>
+      <Text>404 Not Found</Text>
+      <Text>페이지를 찾을 수 없습니다.</Text>
+    </Wrapper>
+  );
 };
+
+const Text = styled.div`
+  color: black;
+  font-size: 1rem;
+  &:not(:last-child) {
+    margin-bottom: 1rem;
+    font-size: 2rem;
+    color: #371ff0;
+    font-weight: 700;
+  }
+`;
+
+const Wrapper = styled.div`
+  height: 100dvh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
