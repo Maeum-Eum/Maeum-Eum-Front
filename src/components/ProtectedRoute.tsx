ee99e0a815b8249135b68eae03b0af3ba71216e4
zziyuni <86959644+zziyuni@users.noreply.github.com>
2025-02-20 10:29:44 +0900

    조건문 오류 수정

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
 export const ProtectedRoute = () => {
-  const isLogin = localStorage.getItem('accessToken') === undefined;
+  const isLogin = localStorage.getItem('accessToken') !== undefined;
 
   return isLogin ? <Outlet /> : <Navigate to="/login" replace />;
 };
